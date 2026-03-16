2bc6729c29ad0cf22465883ca142bc24a3ea20af
潘钜森 <2858727468@qq.com>
2026-03-16 22:06:04 +0800

    Add files via upload

---
+++ assets/js/data.js
@@ -0,0 +1,440 @@
+const links = [
+    {
+        "title": "geoisam@qq.com",
+        "icon": '<i class="ri-mail-fill"></i>',
+        "link": "mailto:geoisam@qq.com"
+    },
+    {
+        "title": "QQ",
+        "icon": '<i class="ri-qq-fill"></i>',
+        "link": "https://qm.qq.com/cgi-bin/qm/qr?k=jKiPGENUUyTne98ltuOEDDFfBP1uyAzz"
+    },
+    {
+        "title": "GitHub",
+        "icon": '<i class="ri-github-fill"></i>',
+        "link": "https://github.com/geoisam"
+    },
+    {
+        "title": "Gitee",
+        "icon": '<i class="ri-google-fill"></i>',
+        "link": "https://gitee.com/geoisam"
+    },
+    {
+        "title": "cnblogs",
+        "icon": '<i class="ri-blogger-fill"></i>',
+        "link": "https://home.cnblogs.com/u/geoisam"
+    },
+    {
+        "title": "bilibili",
+        "icon": '<i class="ri-bilibili-fill"></i>',
+        "link": "https://space.bilibili.com/408786204"
+    },
+]
+
+
+const friends = [
+    {
+        "name": "潘钜森的主页",
+        "desc": "愿你一生欢喜，不为世俗所及",
+        "icon": "https://q1.qlogo.cn/g?b=qq&nk=2858727468&s=0",
+        "link": "https://geoisam.github.io",
+        "email": "geoisam@qq.com"
+    },
+    {
+        "name": "暮光工具箱",
+        "desc": "科技便携生活",
+        "icon": "https://qisuhe.github.io/assets/img/logo.svg",
+        "link": "https://qisuhe.github.io",
+        "email": "QiSHuHe@88.com"
+    },
+]
+
+
+const skills = [
+    {
+        "name": "HTML/CSS",
+        "desc": "制作网页的能力：复制粘贴",
+        "progress": 75,
+    },
+    {
+        "name": "JavaScript",
+        "desc": "现用现学的能力：我太菜了",
+        "progress": 45,
+    },
+    {
+        "name": "Python",
+        "desc": "高级语言的能力：会运行",
+        "progress": 30,
+    },
+    {
+        "name": "PHP",
+        "desc": "优雅语言的能力：老是被肏",
+        "progress": 20,
+    },
+    {
+        "name": "Java",
+        "desc": "小而美语言的能力：一点点",
+        "progress": 10,
+    },
+    {
+        "name": "Kotlin",
+        "desc": "安卓开发语言的能力：问AI",
+        "progress": 1,
+    },
+    {
+        "name": "憨属性",
+        "desc": "我可真是个大聪明",
+        "progress": 85,
+    },
+    {
+        "name": "搞事情",
+        "desc": "让事情成功发生的能力",
+        "progress": 50,
+    },
+    {
+        "name": "恋爱脑",
+        "desc": "对爱人的情感深度",
+        "progress": 99,
+    },
+    {
+        "name": "社交",
+        "desc": "熟人<=社恐 | 陌生人<=社牛",
+        "progress": 50,
+    },
+    {
+        "name": "耐心",
+        "desc": "对生物/事情的耐心程度",
+        "progress": 30,
+    },
+    {
+        "name": "世界",
+        "desc": "对这个世界热爱的程度",
+        "progress": 60,
+    },
+]
+
+
+const sponsor = [
+    {
+        "time": 1773135773,
+        "name": "千与千寻",
+        "message": "360缺少i18n目录导致无右键",
+        "payment": "WeCaht",
+        "amount": 1.00
+    },
+    {
+        "time": 1770702289,
+        "name": "吕建国",
+        "message": "感谢大佬",
+        "payment": "WeCaht",
+        "amount": 5.00
+    },
+    {
+        "time": 1770532856,
+        "name": "名字",
+        "message": null,
+        "payment": "WeCaht",
+        "amount": 0.10
+    },
+    {
+        "time": 1768197189,
+        "name": "寻川",
+        "message": null,
+        "payment": "WeCaht",
+        "amount": 132.00
+    },
+    {
+        "time": 1767400935,
+        "name": "蚯。。蚓",
+        "message": null,
+        "payment": "WeCaht",
+        "amount": 1.00
+    },
+    {
+        "time": 1766816650,
+        "name": "Buried dreams",
+        "message": "支持一下",
+        "payment": "WeCaht",
+        "amount": 20.00
+    },
+    {
+        "time": 1766494870,
+        "name": "(ง •̀_•́)ง",
+        "message": "很强/:strong",
+        "payment": "WeCaht",
+        "amount": 10.00
+    },
+    {
+        "time": 1766158954,
+        "name": "齐澍鹤",
+        "message": null,
+        "payment": "WeCaht",
+        "amount": 30.00
+    },
+    {
+        "time": 1765105511,
+        "name": null,
+        "message": null,
+        "payment": "WeCaht",
+        "amount": 0.10
+    },
+    {
+        "time": 1763449456,
+        "name": null,
+        "message": null,
+        "payment": "WeCaht",
+        "amount": 0.30
+    },
+    {
+        "time": 1762690497,
+        "name": "布帆",
+        "message": "非常棒，加油",
+        "payment": "WeCaht",
+        "amount": 1.00
+    },
+    {
+        "time": 1761388471,
+        "name": null,
+        "message": null,
+        "payment": "WeCaht",
+        "amount": 10.00
+    },
+    {
+        "time": 1761056572,
+        "name": "**鹏",
+        "message": null,
+        "payment": "Alipay",
+        "amount": 10.00
+    },
+    {
+        "time": 1757920003,
+        "name": null,
+        "message": "感谢开发脚本",
+        "payment": "WeCaht",
+        "amount": 10.00
+    },
+    {
+        "time": 1757636426,
+        "name": "黑猫",
+        "message": "谢谢哥哥的脚本",
+        "payment": "WeCaht",
+        "amount": 3.00
+    },
+    {
+        "time": 1757172774,
+        "name": "一百个",
+        "message": "感谢分享小米管家补丁",
+        "payment": "WeCaht",
+        "amount": 2.00
+    },
+    {
+        "time": 1755608533,
+        "name": "克勤克俭",
+        "message": "谢谢老哥",
+        "payment": "WeCaht",
+        "amount": 5.00
+    },
+    {
+        "time": 1753709247,
+        "name": "王翱翔律师",
+        "message": null,
+        "payment": "WeChat",
+        "amount": 1.00
+    },
+    {
+        "time": 1753294587,
+        "name": null,
+        "message": null,
+        "payment": "WeChat",
+        "amount": 0.50
+    },
+    {
+        "time": 1751880358,
+        "name": "愿无二世",
+        "message": null,
+        "payment": "WeChat",
+        "amount": 10.00
+    },
+    {
+        "time": 1750300459,
+        "name": "mhsj",
+        "message": "感谢哥哥",
+        "payment": "WeChat",
+        "amount": 200.00
+    },
+    {
+        "time": 1749439676,
+        "name": "几度窥月",
+        "message": null,
+        "payment": "Alipay",
+        "amount": 1.00
+    },
+    {
+        "time": 1749259276,
+        "name": "靠谱",
+        "message": "大哥，可以给个联系方式吗",
+        "payment": "WeChat",
+        "amount": 1.00
+    },
+    {
+        "time": 1746861674,
+        "name": "ia",
+        "message": "nb 感谢大佬",
+        "payment": "WeChat",
+        "amount": 2.00
+    },
+    {
+        "time": 1746768413,
+        "name": null,
+        "message": null,
+        "payment": "WeChat",
+        "amount": 8.88
+    },
+    {
+        "time": 1746266756,
+        "name": "Zzz",
+        "message": "感谢大佬分享，希望能分享手机端使用什么浏览器用于脚本执行谢谢",
+        "payment": "WeChat",
+        "amount": 3.00
+    },
+    {
+        "time": 1746084135,
+        "name": null,
+        "message": null,
+        "payment": "WeChat",
+        "amount": 0.30
+    },
+    {
+        "time": 1744258248,
+        "name": "高",
+        "message": "🐮🐮🐮",
+        "payment": "WeChat",
+        "amount": 3.00
+    },
+    {
+        "time": 1743936792,
+        "name": "陈昌烨",
+        "message": null,
+        "payment": "WeChat",
+        "amount": 5.25
+    },
+    {
+        "time": 1742617385,
+        "name": "之慕",
+        "message": "很好用的脚本😁",
+        "payment": "WeChat",
+        "amount": 5.00
+    },
+    {
+        "time": 1740766172,
+        "name": "Geber",
+        "message": null,
+        "payment": "WeChat",
+        "amount": 10.00
+    },
+    {
+        "time": 1740734933,
+        "name": "维C",
+        "message": "领到一百米的天猫超市礼品卡了，舒服了",
+        "payment": "WeChat",
+        "amount": 5.00
+    },
+    {
+        "time": 1739978637,
+        "name": null,
+        "message": null,
+        "payment": "WeChat",
+        "amount": 3.00
+    },
+    {
+        "time": 1739344031,
+        "name": "几度窥月",
+        "message": null,
+        "payment": "Alipay",
+        "amount": 1.00
+    },
+    {
+        "time": 1738154189,
+        "name": "无妄",
+        "message": "脚本猫论坛~ 李白",
+        "payment": "WeChat",
+        "amount": 8.00
+    },
+    {
+        "time": 1738080772,
+        "name": "苏哲",
+        "message": "我爱你",
+        "payment": "WeChat",
+        "amount": 5.20
+    },
+    {
+        "time": 1738070692,
+        "name": "鬱璟宸",
+        "message": "新年快乐",
+        "payment": "WeChat",
+        "amount": 10.00
+    },
+    {
+        "time": 1734545543,
+        "name": "ljm",
+        "message": null,
+        "payment": "WeChat",
+        "amount": 1.00
+    },
+    {
+        "time": 1734013644,
+        "name": "K3",
+        "message": null,
+        "payment": "WeChat",
+        "amount": 1.00
+    },
+    {
+        "time": 1733233449,
+        "name": "想喝冰可乐",
+        "message": "哇！哥哥的脚本好棒！",
+        "payment": "WeChat",
+        "amount": 5.00
+    },
+    {
+        "time": 1723160668,
+        "name": "齐澍鹤",
+        "message": null,
+        "payment": "WeChat",
+        "amount": 20.00
+    },
+    {
+        "time": 1722329420,
+        "name": "齐澍鹤",
+        "message": null,
+        "payment": "WeChat",
+        "amount": 10.00
+    },
+    {
+        "time": 1717676557,
+        "name": "饼哥",
+        "message": "请你喝奶茶",
+        "payment": "WeChat",
+        "amount": 25.00
+    },
+    {
+        "time": 1714569084,
+        "name": "陈昌烨",
+        "message": null,
+        "payment": "WeChat",
+        "amount": 1.00
+    },
+    {
+        "time": 1713072301,
+        "name": "齐澍鹤",
+        "message": null,
+        "payment": "WeChat",
+        "amount": 6.66
+    },
+    {
+        "time": 1712375844,
+        "name": "齐澍鹤",
+        "message": null,
+        "payment": "WeChat",
+        "amount": 20.00
+    },
+]
